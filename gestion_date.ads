7a5e2e9fe696cef9fd209fe665c96d65515253d1
tchanaerwanfarel-lgtm <tchanaerwanfarel@gmail.com>
2026-04-11 13:37:01 +0200

    Create gestion_date.ads

---
+++ gestion_date.ads
@@ -0,0 +1,25 @@
+with ada.text_io, ada.integer_text_io;
+use  ada.text_io, ada.integer_text_io;
+
+package  gestion_date is
+
+type T_nom_mois is (janvier, fevrier, mars, avril, mai, juin, juillet, aout, septembre, octobre, novembre, decembre);
+
+type T_mois is record
+M: T_nom_mois;
+nj:integer range 1..31;
+end record;
+
+type T_tab_mois is array (T_nom_mois) of integer;
+
+type T_date is record
+ j : integer;
+ M: T_nom_mois;
+ annee: integer;
+end record;
+
+procedure saisirdate (D: out T_date) ;
+PROCEDURE Visudate(D: IN T_Date);
+FUNCTION Lendemain(D :  T_Date) RETURN T_DATE;
+function avantD(D1,D2: T_date) return boolean;
+end gestion_date;
